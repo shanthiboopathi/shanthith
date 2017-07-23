b3694e0250815c77762975122ce7cd2967fbea14
shanthiboopathi <30317727+shanthiboopathi@users.noreply.github.com>
2017-07-23 16:36:29 +0530

    Create paragraph.c

---
+++ paragraph.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+unsigned int long linecount;
+ linecount=0;
+ while((ch=getc(stdin))!=EOF)
+ {
+       if (ch=='\n') {++linecount;} }
+ if(line count>0){
+  ++linecount;
+ }
+printf( "%lu \n", linecount );
+return 0;
+}
